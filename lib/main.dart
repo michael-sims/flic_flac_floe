5e1433d17f3cb967d802971b11e627522499b8a9
Michael Sims <michael_a_sims@yahoo.com>
2018-01-29 21:21:57 -0800

    Handle assigning owners to the cells.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flic_flac_floe/game.dart';
 
-void main() {
-  runApp(new FlicFlacFloeApp());
-}
+void main() => runApp(new FlicFlacFloeApp());
 
 class FlicFlacFloeApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -26,44 +24,97 @@ class GameScreen extends StatefulWidget {
 }
 
 class GameScreenState extends State<GameScreen> {
+
+  final List<Player> _players = [new Player("X"), new Player("O")];
+  final List<Move> _moves = new List<Move>();
+
+  void move(final int x, final int y) {
+    _moves.add(new Move(_players.first, x, y));
+    _cyclePlayers();
+  }
+
+  void _cyclePlayers() {
+    Player p = _players.removeAt(0);
+    _players.add(p);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final GameBoard board = new GameBoard(_moves);
+
     return new Scaffold(
         appBar: new AppBar(
             title: new Text("Play ball!")
         ),
-        body: _buildGameGrid(context)
+        body: _buildGameGrid(context, board)
     );
   }
 
 
-  Widget _buildGameGrid(BuildContext context) {
+  Widget _buildGameGrid(BuildContext context, final GameBoard board) {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
       child: new Row(
-          children: new List.generate(GameBoard.WIDTH, (x) => _row(x))
+          children: new List.generate(GameBoard.WIDTH, (x) => _row(x, board))
       ),
     );
   }
 
-  Widget _row(int x) {
+  Widget _row(int x, final GameBoard board) {
     return new Column(
-      children: new List.generate(GameBoard.HEIGHT, (y) => new GridCell(x, y)),
+      children: new List.generate(
+          GameBoard.HEIGHT, (y) => _cellAt(x, y, board)),
     );
+  }
+
+  Widget _cellAt(int x, int y, final GameBoard board) {
+    Player owner = board.ownerOf(x, y);
+    return new GridCell(x, y, owner, () {
+      setState(() {
+        move(x, y);
+      });
+    });
   }
 }
 
 class GridCell extends StatelessWidget {
-  GridCell(this.locX, this.locY);
+  GridCell(this.locX,
+      this.locY,
+      this.owner,
+      this.onPressed);
 
   final int locX;
   final int locY;
+  final Player owner;
+  final VoidCallback onPressed;
+
 
   @override
   Widget build(BuildContext context) {
     return new Container(
         margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-        child: new Text("$locX,$locY")
+        child: new Column(
+          children: <Widget>[
+
+            new IconButton(
+                icon: new Icon(fromPlayer(owner)),
+                onPressed: owner == Player.NONE ? onPressed : null
+            ),
+            new Text("$locX,$locY"),
+          ],
+        )
     );
+  }
+
+  IconData fromPlayer(Player p) {
+    if (p.name == "X") {
+      return Icons.close;
+    }
+
+    if (p.name == "O") {
+      return Icons.add_circle;
+    }
+
+    return Icons.cake;
   }
 }
